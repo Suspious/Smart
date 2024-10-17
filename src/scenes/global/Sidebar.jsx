ade2e3546b00f6ba727ee6ff23d9222ca977505b
hobjes <anthonyzakelijk@outlook.com>
2024-10-17 10:12:12 +0200

    here

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+ import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
@@ -94,7 +94,7 @@ const Sidebar = () => {
             )}
           </MenuItem>
 
-          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+          <Box padsdingLeft={isCollapsed ? undefined : "10%"}>
             <Typography  
               variant="h6"
               color="#B7B7B7"
